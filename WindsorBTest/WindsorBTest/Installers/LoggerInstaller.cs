eb4e1c27a5ee63325f6afc562a44e0b19cf7e2f9
Ermin Husovic <ermin.husovic@gmail.com>
2013-01-20 16:54:24 +0100

    Adding Castle Windsor logging facility

---
+++ WindsorBTest/WindsorBTest/Installers/LoggerInstaller.cs
@@ -0,0 +1,15 @@
+﻿namespace WindsorBTest.Installers
+{
+    using Castle.Facilities.Logging;
+    using Castle.MicroKernel.Registration;
+    using Castle.MicroKernel.SubSystems.Configuration;
+    using Castle.Windsor;
+
+    public class LoggerInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(f => f.UseLog4Net());
+        }
+    }
+}
